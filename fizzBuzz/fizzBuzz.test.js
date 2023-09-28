76f6e173a302b02690ac22fd8a4b9d1e276438e7
Qixin YING <ying.qixin@gmail.com>
2023-09-28 10:02:08 +0200

    fix: small test error in fizzbuzz

--- fizzBuzz/fizzBuzz.test.js
+++ fizzBuzz/fizzBuzz.test.js
@@ -14,13 +14,13 @@ describe('FizzBuzz', () => {
     assert.equal(result, 1);
   });
 
-  it('Should return 99 if num equals 99', () => {
-    //Given num is 1
-    let num = 99;
+  it('Should return 4 if num equals 4', () => {
+    //Given num is 4
+    let num = 4;
     //When 
     let result = fizzBuzz(num);
     //Then
-    assert.equal(result, 99);
+    assert.equal(result, 4);
   });
   
 });
